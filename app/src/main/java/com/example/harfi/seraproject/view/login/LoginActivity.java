5a4837a777b8a288be8f33c56ddd4690e69167a8
harfinovian <harfinovian@yahoo.com>
2017-05-15 15:39:55 +0700

    login & splash logic

--- app/src/main/java/com/example/harfi/seraproject/view/login/LoginActivity.java
+++ app/src/main/java/com/example/harfi/seraproject/view/login/LoginActivity.java
@@ -1,10 +1,86 @@
 package com.example.harfi.seraproject.view.login;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.harfi.seraproject.R;
 import com.example.harfi.seraproject.base.BaseActivity;
+import com.example.harfi.seraproject.view.main.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by harfi on 5/15/2017.
  */
 
 public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.buttonSignIn) Button btn_signIn;
+    @BindView(R.id.editUserName) EditText et_email;
+    @BindView(R.id.editPassword) EditText et_password;
+    @BindView(R.id.btnToSignUp) TextView btn_toSignUp;
+    Intent intent;
+    private FirebaseAuth mAuth;
+    private final static String TAG="SignIn";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    /*@OnClick(R.id.btnToSignUp)
+    public void onClickSignUp(View v) {
+        intent = new Intent(SignIn.this, SignUp.class);
+        startActivity(intent);
+    }*/
+
+    @OnClick(R.id.buttonSignIn)
+    public void onClickSignIn(View v) {
+        if (et_email.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Email must be filled!", Toast.LENGTH_SHORT).show();
+        } else if (et_password.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Password must be filled!", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signInWithEmailAndPassword(et_email.getText().toString(), et_password.getText().toString())
+                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                            if (!task.isSuccessful()) {
+                                Log.w(TAG, "signInWithEmail:failed", task.getException());
+                                Toast.makeText(LoginActivity.this, "failed",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this).edit();
+                                editor.putString("email", et_email.getText().toString());
+                                editor.putString("password", et_password.getText().toString());
+                                editor.putBoolean("login", true);
+                                editor.commit();
+                                intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+        }
+    }
 }
--- app/src/main/java/com/example/harfi/seraproject/view/splash/SplashActivity.java
+++ app/src/main/java/com/example/harfi/seraproject/view/splash/SplashActivity.java
@@ -1,10 +1,128 @@
 package com.example.harfi.seraproject.view.splash;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.example.harfi.seraproject.R;
 import com.example.harfi.seraproject.base.BaseActivity;
+import com.example.harfi.seraproject.view.login.LoginActivity;
+import com.example.harfi.seraproject.view.main.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by harfi on 5/13/2017.
  */
 
 public class SplashActivity extends BaseActivity{
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private final static String TAG ="AuthLog";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("login", false))
+        {
+            Log.d("Login","Null");
+            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+            splashThread(i);
+        }
+        else
+        {
+            if(prefs.getString("email",null) != null && prefs.getString("password", null) != null) {
+                Log.d("Login","Success");
+                checkLogin(prefs.getString("email",null),prefs.getString("password", null));
+            }else{
+                Toast.makeText(SplashActivity.this, "Username or password must insert",
+                        Toast.LENGTH_SHORT).show();
+                Log.d("Login","Failed");
+            }
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void splashThread(final Intent i){
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    private void checkLogin(final String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SplashActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        System.out.print("Test" + email);
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(SplashActivity.this, "User name or Email is invalid",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                            splashThread(i);
+                        } else {
+                            Toast.makeText(SplashActivity.this, "Login Successfully",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                            splashThread(i);
+                        }
+                    }
+                });
+    }
+    
 }
