5f08cae6e739b024107fe375506b2d3502de788d
harfinovian <harfinovian@yahoo.com>
2017-05-15 15:17:01 +0700

    firebase config

--- app/src/main/java/com/example/harfi/seraproject/view/splash/SplashActivity.java
+++ app/src/main/java/com/example/harfi/seraproject/view/splash/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.example.harfi.seraproject.view.splash;
 
+import com.example.harfi.seraproject.base.BaseActivity;
+
 /**
  * Created by harfi on 5/13/2017.
  */
 
-public class SplashActivity {
+public class SplashActivity extends BaseActivity{
 }
